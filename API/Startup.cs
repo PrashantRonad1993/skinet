3acce9cbb174563abb97c520f3ce3dd9c1ea11bb
prashantronad <uvceprashant@gmail.com>
2022-02-20 14:33:10 +0530

    End of session 13

--- API/Startup.cs
+++ API/Startup.cs
@@ -8,6 +8,7 @@ using API.Helpers;
 using API.MiddleWare;
 using Core.Interfaces;
 using Infrastructure.Data;
+using StackExchange.Redis;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
@@ -33,9 +34,15 @@ namespace API
         public void ConfigureServices(IServiceCollection services)
         {            
             services.AddAutoMapper(typeof(MappingProfiles));
-            services.AddControllers();            
+            services.AddScoped<IBasketRepository,BasketRepository>();
+            services.AddControllers();                   
             services.AddDbContext<StoreContext>(x => 
                 x.UseSqlite(_config.GetConnectionString("DefaultConnection")));
+            services.AddSingleton<IConnectionMultiplexer>(c =>{
+                var configuration = ConfigurationOptions.Parse(_config.
+                GetConnectionString("Redis"),true);
+                return ConnectionMultiplexer.Connect(configuration);
+            });
             services.AddApplicationServices();              
             services.AddSwaggerDocumentation();
             services.AddCors(options =>
